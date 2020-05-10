c5d97a22c0395b3b3688525f5c8fc6ad853405d3
Hsin-chan Chien <hcchien@gmail.com>
2020-05-10 19:42:20 +0800

    update fields

--- index.js
+++ index.js
@@ -24,20 +24,26 @@ const userIsAdminOrOwner = auth => {
   const isOwner = access.userOwnsItem(auth);
   return isAdmin ? isAdmin : isOwner;
 };
+const status_options = [
+  { value: 'active', label: "active" },
+  { value: 'inactive', label: "inactive" },
+];
 const everyone = ({ authentication: { item: user } }) => Boolean(user);
 const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner, everyone };
 keystone.createList('User', {
   fields: {
-    name: { type: Text },
+    name: { label: "姓名", type: Text, isRequired: true },
+    phone: { label: "電話", type: Text, isRequired: true },
     email: {
       type: Text,
       isUnique: true,
     },
-        role: { type: Select, options: 'users, guest' },
+    role: { type: Select, options: 'users, guest' },
     isAdmin: { type: Checkbox },
     password: {
       type: Password,
     },
+    user_status: { label: "狀態", type: Select, options: status_options, isRequired: true},
   },
   access: {
     read: access.userIsAdminOrOwner,

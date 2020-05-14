88ca3d7f34bc817cbfbc0f2bf056b296f5f7a23b
Hsin-chan Chien <hcchien@gmail.com>
2020-05-14 11:08:54 +0800

    remove IsUser from permission

--- lists/Permission.js
+++ lists/Permission.js
@@ -3,6 +3,6 @@ const userIsSales = ({ authentication: { item: user } }) => Boolean(user && (use
 const userIsExecutor = ({ authentication: { item: user } }) => Boolean(user && (user.role == 'executor' || user.role == 'sales' || user.role == 'admin' ));
 const userIsPlanner = ({ authentication: { item: user } }) => Boolean(user && (user.role == 'planner' || user.role == 'executor' || user.role == 'sales' || user.role == 'admin' ));
 const everyone = ({ authentication: { item: user } }) => Boolean(user);
-const access = { userIsAdmin, userIsUsers, everyone, userIsSales, userIsExecutor, userIsPlanner };
+const access = { userIsAdmin, everyone, userIsSales, userIsExecutor, userIsPlanner };
 
 module.exports = { "access": access }

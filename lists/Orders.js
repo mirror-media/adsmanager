82231ab8a11f6f3c21670f3d2683016cae51b4fb
Hsin-chan Chien <hcchien@gmail.com>
2020-07-10 16:02:03 +0800

    update some fields

--- lists/Orders.js
+++ lists/Orders.js
@@ -7,6 +7,11 @@ const options = [
   { value: 'end', label: "結束" },
 ];
 
+const payment_options = [
+  { value: 'cash', label: "出刊前付現" },
+  { value: '30', label: "月結30天" },
+  { value: '60', label: "月結60天" },
+];
 const status_options = [
   { value: 'draft', label: "草稿" },
   { value: 'await', label: "待審核" },
@@ -20,13 +25,12 @@ module.exports = {
   plural: "委刊單",
   fields: {
     adname: { label: "廣告名稱", type: Text, isRequired: true},
-    clientname: { label: "客戶ID", type: Relationship, many: false, ref: 'Client',  isRequired: true},
-    username: { label: "使用者ID", type: Relationship, many: false, ref: 'User',  isRequired: true},
-    commissions: { label: "委刊項ID", type: Relationship, many: true, ref: 'Z_Commission',  isRequired: true},
+    clientname: { label: "廣告客戶", type: Relationship, many: false, ref: 'Client',  isRequired: true},
+    username: { label: "訂版人員", type: Relationship, many: false, ref: 'User',  isRequired: true},
     order_status: { label: '狀態', type: Select, options: status_options, isRequired: true, default: 'draft' },
-    start_date: { label: "開始日期", type: DateTime, yearRangeFrom: 2020, yearRangeTo: 2022, yearPickerType: 'select', isRequired: true},
-    end_date: { label: "結束日期", type: DateTime, yearRangeFrom: 2020, yearRangeTo: 2022, yearPickerType: 'select', isRequired: true},
-    period: { label: "期數／天數", type: Integer, isRequired: false},
+    start_date: { label: "開始日", type: DateTime, yearRangeFrom: 2020, yearRangeTo: 2022, yearPickerType: 'select', isRequired: true},
+    end_date: { label: "結束日", type: DateTime, yearRangeFrom: 2020, yearRangeTo: 2022, yearPickerType: 'select', isRequired: true},
+    commissions: { label: "委刊項", type: Relationship, many: true, ref: 'Z_Commission',  isRequired: true},
     tax_free: { 
 		label: "總金額（未稅）", type: Integer, isRequired: false,
 		hooks: {
@@ -38,7 +42,7 @@ module.exports = {
 		}
 	},
     tax_included: { label: "總金額（含稅）", type: Integer, isRequired: false},
-    payment: { label: "付款條件", type: Text, isRequired: false},
+    payment: { label: "付款條件", type: Select, options: payment_options, isRequired: false},
     remark: { label: "其他備註", type: Text, isRequired: false},
   },
   access: {

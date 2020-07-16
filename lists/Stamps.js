a47b77133fa618b1af4a8e7416132c991231b14e
Hsin-chan Chien <hcchien@gmail.com>
2020-07-16 13:11:25 +0800

    upadte platform and section in Stamp with relationship

--- lists/Stamps.js
+++ lists/Stamps.js
@@ -1,35 +1,10 @@
 const { access } = require('./Permission.js');
-const { Text, Select, Integer, Float } = require('@keystonejs/fields');
+const { Text, Select, Integer, Float, Relationship } = require('@keystonejs/fields');
 const { atTracking } = require('@keystonejs/list-plugins');
 
 const status_options = [
   { value: 'active', label: "active" },
   { value: 'inactive', label: "inactive" },
-];
-const platform_options = [
-  { value: 'desktop', label: "桌機廣告" },
-  { value: 'mobile', label: "手機廣告" },
-  { value: 'cross', label: "跨裝置廣告" },
-  { value: 'native', label: "原生廣告" },
-  { value: 'article', label: "數位廣編" },
-  { value: 'fb', label: "Facebook" },
-  { value: 'line', label: "line" },
-  { value: 'ig', label: "IG" },
-];
-
-const section_options = [
-  { value: 'all', label: "全站不指定" },
-  { value: 'news', label: "時事" },
-  { value: 'ent', label: "娛樂" },
-  { value: 'finance', label: "財經理財" },
-  { value: 'people', label: "人物" },
-  { value: 'video', label: "影音" },
-  { value: 'intl', label: "國際" },
-  { value: 'food', label: "美食旅遊" },
-  { value: 'mafada', label: "瑪法達" },
-  { value: 'culture', label: "文化" },
-  { value: 'watch', label: "汽車鐘錶" },
-  { value: 'other', label: "其他" },
 ];
 
 const charge_options = [
